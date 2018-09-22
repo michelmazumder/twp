6adf9a51865db5c86e112f59ec37c12898b5cb69
Michel Mazumder <michel.mazumder@icloud.com>
2018-09-22 14:23:57 +0200

    tmp

--- src/twp/Document.h
+++ src/twp/Document.h
@@ -3,11 +3,25 @@
 #include <string>
 #include <list>
 #include <memory>
+#include <vector>
+
 #include "win/Window.h"
 #include "tdm/AttributedRope.h"
 #include "util/Debuggable.h"
 
 namespace twp {
+
+	class ViewPort {
+		size_t firstPositionOnRope;
+		std::vector<std::string> lines;
+
+		// riga x colonna x valore attributo
+		std::vector<std::vector<std::vector<int>>> attron;
+		std::vector<std::vector<std::vector<int>>> attroff;
+
+	public:
+		void render(win::Window& w) const;
+	};
 
 	class Document {
 
